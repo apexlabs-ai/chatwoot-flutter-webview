33bc3dd2e7ced7a03855ef82c775ec03dfb110b2
Ajil Oommen <ajilo297@gmail.com>
2021-08-05 12:07:35 +0530

    Removed default file names

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,24 +2,19 @@ import 'package:chatwoot_flutter_webview/chatwoot_flutter_webview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
+      home: ChatwootDemoPage(),
+    ),
+  );
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super(key: key);
+class ChatwootDemoPage extends StatelessWidget {
+  ChatwootDemoPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
